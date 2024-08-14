4e973f9f3ae33a68d38ed2ec7eee4eb73df15bbc
BrayanDEV <brayandaniel_64@hotmail.com>
2024-08-14 11:17:59 -0500

    Deleting jenkins file

---
+++ Jenkinsfile/jenkinsfile.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mvn clean install'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+    }
+    post {
+        always {
+            junit '**/target/surefire-reports/*.xml'
+            archiveArtifacts artifacts: '**/target/*.jar', allowEmptyArchive: true
+        }
+    }
+}
