0b80af602062a39d60bf076e83d4b59a5d08b008
BrayanDEV <brayandaniel_64@hotmail.com>
2024-08-17 00:06:35 -0500

    Updating jenkinsfile to recive parameters TAGS

--- Jenkinsfile/jenkinsfile1.groovy
+++ Jenkinsfile/jenkinsfile1.groovy
@@ -1,6 +1,10 @@
 pipeline {
     agent any
 
+    parameters {
+        string(name: 'TEST_TAG', defaultValue: 'smoke', description: 'Tag de las pruebas a ejecutar')
+    }
+
     stages {
         stage('Build') {
             steps {
@@ -12,7 +16,17 @@ pipeline {
         stage('Test') {
             steps {
                 echo 'Running tests...'
-                bat 'mvn test'
+                //bat 'mvn test'
+                //bat 'mvn verify -Dcucumber.options="--plugin json:target/cucumber-report/cucumber.json"'
+                script {
+                    // Obtener el valor del parámetro ingresado
+                    def tag = params.TEST_TAG
+                    echo "Running tests with tag: ${tag}"
+
+                    // Comando para ejecutar las pruebas filtrando por el tag
+                    // Este ejemplo usa Maven y Cucumber, ajústalo según tu proyecto
+                    bat 'mvn test -Dcucumber.options="--tags @${tag} --plugin json:target/cucumber-report/cucumber.json"'
+                }
             }
         }
 
